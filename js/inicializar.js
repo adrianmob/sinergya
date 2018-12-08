97e8954ee96aa183e076f95e815803c8e4d1ecbc
Jesus Adrian Quiroz Palacios <mobvi68@gmail.com>
2018-12-08 13:39:46 -0600

    empezamos

---
+++ js/inicializar.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyDkIbkL1iQ6qJJ9cPjT-Em8oqItCKA7rHo",
+    authDomain: "conacceso-f6a96.firebaseapp.com",
+    databaseURL: "https://conacceso-f6a96.firebaseio.com",
+    projectId: "conacceso-f6a96",
+    storageBucket: "conacceso-f6a96.appspot.com",
+    messagingSenderId: "79779549797"
+  };
+  firebase.initializeApp(config);
